4d4e16946a0ce5f134c9b8061a7a00184f4e14fb
王亭然 <wangtingran@kuaishou.com>
2024-09-22 15:46:18 +0800

    创建项目

---
+++ gitProj/gitProj/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gitProj
+//
+//  Created by tingran wang on 2024/9/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
